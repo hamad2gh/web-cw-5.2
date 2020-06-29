7d10fc4375f5fcd7a0f0de8bf2364fd711b19ad0
hamad <hamad2030g@gmail.com>
2020-06-30 00:06:59 +0300

    تم الانتهاء

---
+++ Development/js/scripts.js
@@ -0,0 +1,65 @@
+// const food = prompt("ماذا تحب ان تطلب ؟") ;
+
+// if (food == 1) { console.log( "بيتزا")
+
+// }
+// if (food == 2) { console.log( "برقر")
+
+// }
+// if (food == 3) { console.log( "سوشي")
+
+// }
+// if (food == 4) { console.log( "طلبك غير متوفر")
+
+// }
+
+//  هذا البونص 
+// switch(food) {
+//     case("1"):
+//       (console.log("سوشي"))
+//       break;
+//     case ("2"):
+//       (console.log("برقر"))
+//       break;
+//       case ("3"):
+//       (console.log("سوشي"))
+//       break;
+//     default:(console.log("طلبك غير متوفر"))
+//     break;
+      
+//   }
+
+// let abc = prompt(" ادخل الرقم الاول") 
+
+// let efg = prompt(" ادخل الرقم الثاني") ;
+
+// let hij = prompt("اختر العملية (* / - +)") ;
+
+
+// const x = parseInt(abc) ;
+// const y = parseInt(efg) ;
+
+// if (x && y)
+// {
+// if(hij=="*"){console.log(x*y)}
+// else if(hij=="-"){console.log(x-y)}
+// else if(hij=="+"){console.log(x+y)}
+// else if(hij=="/"){console.log(x/y)}
+
+// else{console.log("العملية غير صحيحه" )}
+// }
+// else {console.log("احد الارقام غير صحيح")}
+
+
+let age = 3
+console.log(age) ;
+if(age >= 4 && age <= 6 )  {
+    console.log("انت في الروضه")
+} 
+else if (age <= 17 && age<=7) {console.log(" انت في المدرسه")
+
+}else if (age < 27 && age >= 18){
+    console.log(" انت في الجامعه ")
+}else {
+    console.log("لا تستوفي الشروط ")
+}
